a8f66d53eff7390cd948e7d9aefa225d1b60c7f0
Angelo Filho <angelo.filho06@hotmail.com>
2023-02-01 18:16:13 -0300

    feat: add radio buttons to new transaction form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,16 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from "./styles";
+
+import * as RadioGroup from "@radix-ui/react-radio-group";
+
+import { ArrowCircleUp, X } from "phosphor-react";
+
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +27,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton value="income" variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton value="outcome" variant="outcome">
+              <ArrowCircleUp size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
